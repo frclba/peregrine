5e8e6e554431d7a9fcf6dfaec95ba69c02a66f5b
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2020-10-12 22:32:12 -0300

    WIP: add initial automated  tweet function

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,15 @@
 import firebase from "./firebase";
+const dotenv = require("dotenv");
+const Twitter = require("./twitter.js");
+
+dotenv.config();
+
+const config = {
+  CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
+  CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET,
+  ACCESS_TOKEN: process.env.TWITTER_ACCESS_TOKEN,
+  ACCESS_TOKEN_SECRET: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+};
 
 export const getCollection = async (name) => {
   const db = firebase.firestore();
@@ -13,3 +24,15 @@ export const getAllFromCollection = async (name) => {
     return doc.data();
   });
 };
+
+console.log("hi twitter");
+
+const twitter = new Twitter(config);
+
+tweet();
+
+async function tweet() {
+  const response = await twitter.tweet(`A2Z 4EVER ${Math.random()}`);
+  console.log(response.id);
+  console.log(response.text);
+}
